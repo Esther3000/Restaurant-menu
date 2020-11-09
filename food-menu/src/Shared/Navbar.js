e0fbb270bfd49788536a2ddbe63ee68952460a95
Esther3000 <esthermaina3000@gmail.com>
2020-11-09 19:49:24 +0300

    update

--- food-menu/src/Shared/Navbar.js
+++ food-menu/src/Shared/Navbar.js
@@ -5,7 +5,7 @@ import './MenuItems.css';
 
 const NavBar = () => (
     <header className='navbar'>
-        <div className='navbar-title navbar-item'>Cutco</div>
+        <div className='navbar-title navbar-item'>Alfonso's</div>
         <div className='navbar-item'>Main dish</div>
         <div className='navbar-item'>Soup</div>
         <div className='navbar-item'>Dessert</div>
